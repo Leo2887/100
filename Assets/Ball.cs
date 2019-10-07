f74ac31b30750087b45a3fa392a77336b9b237d8
Leo2887 <35976728+Leo2887@users.noreply.github.com>
2019-10-07 10:44:45 -0600

    should move object and change size

---
+++ Assets/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb2d;//Rigid body 2D
+    public float speed = 5f;
+    public float scale = 0.1f;
+    public CircleCollider2D myCollider; //to detect collision
+    public string Tag = "ball";
+    // Start is called before the first frame update
+    void Start()
+    {
+        var force = (transform.forward + transform.up) * speed;// initial move
+        //var force =(transform.forward + transform.right) * speed;  you can change the intial move like this
+
+        rb2d = GetComponent<Rigidbody2D>();
+        
+        rb2d.AddForce(force, ForceMode2D.Impulse);//initial move
+        myCollider = GetComponent<CircleCollider2D>();//collision detection
+        rb2d.transform.localScale = new Vector3(1.0f,1.0f, 0);//set the initial size of the ball
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))//detect mouse clic
+        {
+            //detect the position of the input weather it's match to the position of object
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit)//if the input and position of ball matches...
+            {
+                scale += 0.1f;//the amount of increse in size each time you hit object
+                rb2d.transform.localScale = new Vector3(1.0f + scale, 1.0f + scale, 0);
+                Debug.Log("Touched");
+            }
+
+            
+        }
+
+    }
+}
+    
---
+++ Assets/text.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+    public GameObject ball;
+    public float x;
+    public float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = ball.transform.position.x;
+        y = ball.transform.position.y;
+
+        gameObject.transform.position = new Vector3(x,y,0);
+
+    }
+}
